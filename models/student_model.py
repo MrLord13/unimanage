2f777d04a140fe9d7412544918b135dfa6be3381
ali <@li#R3Za>
2025-02-14 14:30:55 +0330

    Add Views And Security CSV

--- models/student_model.py
+++ models/student_model.py
@@ -8,4 +8,6 @@ class UniManageStudent(models.Model):
     name = fields.Char(string='نام و نام خانوادگی', required=True)
     student_code = fields.Char(string='شماره دانشجویی', required=True, unique=True)
     student_number = fields.Char(string='شماره تماس')
+    student_picture = fields.Binary(string='تصویر دانشجو')
+    student_description = fields.Text(string='توضیحات دانشجو')
     class_ids = fields.Many2many('unimanage.class', string='کلاس')
