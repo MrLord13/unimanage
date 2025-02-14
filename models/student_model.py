166dd756c931ff1907b8bd7d1b65f0d452471942
ali <@li#R3Za>
2025-02-14 15:23:34 +0330

    Edit View Alignment Items And Added More Fields In Student Models

--- models/student_model.py
+++ models/student_model.py
@@ -7,6 +7,9 @@ class UniManageStudent(models.Model):
 
     name = fields.Char(string='نام و نام خانوادگی', required=True)
     student_code = fields.Char(string='شماره دانشجویی', required=True, unique=True)
+    student_code_meli = fields.Char(string='کد ملی')
+    student_grade = fields.Char(string='مقطع')
+    student_field = fields.Char(string='رشته')
     student_number = fields.Char(string='شماره تماس')
     student_picture = fields.Binary(string='تصویر دانشجو')
     student_description = fields.Text(string='توضیحات دانشجو')
