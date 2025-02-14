0a8a2f3dcec7d1f60bddbc57137f410b7ead8fd0
ali <@li#R3Za>
2025-02-14 18:05:07 +0330

    Added Some Field To Class Model And Student Constraints

--- models/class_model.py
+++ models/class_model.py
@@ -7,6 +7,10 @@ class UniManageClass(models.Model):
 
     name = fields.Char(string='نام کلاس', required=True)
     class_number = fields.Char(string='شماره کلاس', required=True)
+    class_capacity = fields.Char(string='ظرفیت کلاس', required=True)
+    class_start_date = fields.Char(string='ساعت شروع کلاس')
+    class_end_date = fields.Char(string='ساعت پایان کلاس')
+    class_exam_date = fields.Char(string='تاریخ امتحان کلاس')
     description = fields.Text(string='توضیحات کلاس')
     session_ids = fields.One2many('unimanage.session', 'class_id', string='جلسات')
     student_ids = fields.Many2many('unimanage.student', string='دانشجویان')
--- models/student_model.py
+++ models/student_model.py
@@ -7,10 +7,14 @@ class UniManageStudent(models.Model):
 
     name = fields.Char(string='نام و نام خانوادگی', required=True)
     student_code = fields.Char(string='شماره دانشجویی', required=True, unique=True)
-    student_code_meli = fields.Char(string='کد ملی')
+    student_code_meli = fields.Char(string='کد ملی', unique=True)
     student_grade = fields.Char(string='مقطع')
     student_field = fields.Char(string='رشته')
     student_number = fields.Char(string='شماره تماس')
     student_picture = fields.Binary(string='تصویر دانشجو')
     student_description = fields.Text(string='توضیحات دانشجو')
     class_ids = fields.Many2many('unimanage.class', string='کلاس')
+    _sql_constraints = [
+        ('student_code_meli', 'unique(student_code_meli)', 'این کد ملی وجود دارد'),
+        ('student_code', 'unique(student_code)', 'این شماره انشجویی وجود دارد'),
+    ]
